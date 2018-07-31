562e64c8811eb79cef6eb12bc09f76a3ff96f040
Miguel Salvador <msalvadorp@hotmail.com>
2018-07-30 19:15:18 -0500

    Cambios desde la rama 115

---
+++ ClienteMVC/Controllers/Controladora115Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClienteMVC.Controllers
+{
+    public class Controladora115Controller : Controller
+    {
+        // GET: Controladora115
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
